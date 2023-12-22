2777e682931c9a50198d3a792707cbe683a6d051
Alan Xie <axie5247@uni.sydney.edu.au>
2023-12-22 16:33:24 +1100

    navbar, base, and color scheme

--- sumswebsite/sumswebsite/settings.py
+++ sumswebsite/sumswebsite/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +58,9 @@ ROOT_URLCONF = 'sumswebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates/',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- sumswebsite/tailwind.config.js
+++ sumswebsite/tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './templates/*.html',
+    './static/**/*.js',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'penn-blue-light':'#0B2558',
+        'citrine':'#F2DB00',
+        'penn-blue-dark':'#000034',
+        'azul':'##006FBA',
+        'carolina-blue' : '#85BADE',
+        'ghost-white' : '#F8FAFF',
+      },
+    },
   },
+  purge: ['./templates/*.html', './static/**/*.js'],
   plugins: [],
 }
 
